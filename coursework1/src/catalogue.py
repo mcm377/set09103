2b81cd020cd42a9061c0429be4fd2c5f3d1d81d2
mcm377 <40091038@live.napier.ac.uk>
2015-11-03 12:49:03 +0000

    Added test template to catalogue.py

--- coursework1/src/catalogue.py
+++ coursework1/src/catalogue.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
-def root():
-    return "The default, 'root' route"
+def root(name='Test'):
+    user = name
+    return render_template('hello.html', user=user)
 
 @app.route("/artists/")
 def artists():
