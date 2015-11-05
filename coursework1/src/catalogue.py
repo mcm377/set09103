a06b2a173898050b661a89d4ee72b0637cd7347b
mcm377 <40091038@live.napier.ac.uk>
2015-11-05 18:18:00 +0000

    Changed route for /albums; added album details

--- coursework1/src/catalogue.py
+++ coursework1/src/catalogue.py
@@ -8,7 +8,7 @@ def root():
 
 @app.route("/artists/")
 def artists():
-    artists = sorted(['Avicii', 'Bugzy Malone', 'Calvin Harris', 'Clean Bandit', 'Drake', 'Eri Kitamura', 'Gorgon City', 'Kano', 'The Killers', 'Nero', 'Rudimental', 'TRF'])
+    artists = ['Avicii', 'Bugzy Malone', 'Calvin Harris', 'Clean Bandit', 'Drake', 'Eri Kitamura', 'Gorgon City', 'Kano', 'The Killers', 'Nero', 'Rudimental', 'TRF']
     return render_template('artists.html', artists = artists)
 
 @app.route("/artists/<artist>")
@@ -17,8 +17,8 @@ def get_artist(artist):
 
 @app.route("/albums/")
 def albums():
-    albums = sorted([])
-    return render_template('albums.html')
+    albums = sorted(['True', 'Walk With Me', 'I Created Disco', 'Ready For The Weekend', '18 Months', 'Motion', 'New Eyes', '2010', 'OriginL ClassC', 'Live fron France', 'Thank Me Later', 'Take Care', 'Nothing Was The Same', 'Re;Story', 'Shomei (証×明)', 'Sirens', 'The Crypt', 'Real', 'Home Sweet Home', 'London Town', '140 Grime Street', 'Method To The Meadness', 'Welcome Reality', 'Between II Worlds', 'Home', 'We The Generation', 'Hot Fuss', 'Sams Town', 'Day & Age', 'Battle Born', 'GRAVITY'])
+    return render_template('albums.html', albums = albums)
 
 @app.route("/albums/<album>")
 def get_album(album):
