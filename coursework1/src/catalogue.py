c26bc519b4dfd9ef1becf7f1478b842296220eec
mcm377 <40091038@live.napier.ac.uk>
2015-11-06 12:30:48 +0000

    Changed data structures

--- coursework1/src/catalogue.py
+++ coursework1/src/catalogue.py
@@ -10,7 +10,7 @@ def root():
 
 @app.route("/artists/")
 def artists():
-    artists = albums.keys()
+    artists = albums
     return render_template('artists.html', artists = artists)
 
 @app.route("/artists/<artist>")
@@ -19,7 +19,6 @@ def get_artist(artist):
 
 @app.route("/albums/")
 def albums():
-    albums = sorted(['True', 'Walk With Me', 'I Created Disco', 'Ready For The Weekend', '18 Months', 'Motion', 'New Eyes', '2010', 'OriginL ClassC', 'Live fron France', 'Thank Me Later', 'Take Care', 'Nothing Was The Same', 'Re;Story', u'Shomei (証×明)', 'Sirens', 'The Crypt', 'Real', 'Home Sweet Home', 'London Town', '140 Grime Street', 'Method To The Meadness', 'Welcome Reality', 'Between II Worlds', 'Home', 'We The Generation', 'Hot Fuss', 'Sams Town', 'Day & Age', 'Battle Born', 'GRAVITY'])
     return render_template('albums.html', albums = albums)
 
 @app.route("/albums/<album>")
