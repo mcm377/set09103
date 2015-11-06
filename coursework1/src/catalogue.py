3b38b41405baf6678f168e0feff6704194f022ed
mcm377 <40091038@live.napier.ac.uk>
2015-11-06 13:13:54 +0000

    Changed data structures

--- coursework1/src/catalogue.py
+++ coursework1/src/catalogue.py
@@ -2,7 +2,10 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-albums = {'Avicii': ['True'], 'Bugzy Malone': ['Walk With Me'], 'Calvin Harris': ['I Created Disco', 'Ready For The Weekend', '18 Months', 'Motion'], 'Clean Bandit': ['New Eyes', '2010', 'OriginL ClassC', 'Live fron France'], 'Drake': ['Thank Me Later', 'Take Care', 'Nothing Was The Same'], 'Eri Kitamura': ['Re;Story', u'Shomei (証×明)'], 'Gorgon City': ['Sirens', 'The Crypt', 'Real'], 'Kano': ['Home Sweet Home', 'London Town', '140 Grime Street', 'Method To The Meadness'], 'The Killers': ['Hot Fuss', 'Sams Town', 'Day & Age', 'Battle Born'], 'Nero': ['Welcome Reality', 'Between II Worlds'], 'Rudimental': ['Home', 'We The Generation'], 'TRF': ['GRAVITY']}
+
+artist_albums = {'Avicii': ['True'], 'Bugzy Malone': ['Walk With Me'], 'Calvin Harris': ['I Created Disco', 'Ready For The Weekend', '18 Months', 'Motion'], 'Clean Bandit': ['New Eyes', '2010', 'OriginL ClassC', 'Live fron France'], 'Drake': ['Thank Me Later', 'Take Care', 'Nothing Was The Same'], 'Eri Kitamura': ['Re;Story', u'Shomei (証×明)'], 'Gorgon City': ['Sirens', 'The Crypt', 'Real'], 'Kano': ['Home Sweet Home', 'London Town', '140 Grime Street', 'Method To The Meadness'], 'The Killers': ['Hot Fuss', 'Sam\'s Town', 'Day & Age', 'Battle Born'], 'Nero': ['Welcome Reality', 'Between II Worlds'], 'Rudimental': ['Home', 'We The Generation'], 'TRF': ['GRAVITY']}
+
+artist_tracks = {'Avicii': ['Wake Me Up', 'You Make Me', 'Hey Brother', 'Addicted To You', 'Dear Boy', 'Liar Liar', 'Shame on Me', 'Lay Me Down' , 'Hope There\'s Someone', 'Heart Upon My Sleeve'], 'Bugzy Malone': ['Relegation Riddim', 'Ready to Blow', 'Watch Your Mouth', 'M.E.N', 'Get Gassed', 'Pain', 'Get Paid', 'Pagans', 'Walk With Me'], 'Calvin Harris': ['Acceptable in the 80s', 'The Girls', 'I\'m Not Alone', 'Ready for the Weekend', 'Bounce', 'Feels So Close', 'Summer', 'Outside'], 'Clean Bandit': ['A+E', 'Mozart\'s House', 'Dust Clears', 'Rather Be', 'Extraordinary', 'Come Over', 'Real Love', 'Stronger', 'Disconnect'], 'Drake': ['Forever', 'Over', 'Headlines', 'Make Me Proud', 'The Motto', 'Take Care', 'HYFR', 'Started From The Bottom', 'Hold On, We\'re Going Home', 'Worst Behavior', 'Hotline Bling'], 'Eri Kitamura': ['Before the Moment', 'Tsuyogari', 'Realize', 'Guilty Future', 'Be Starters!', 'Shirushi', 'Happy Girl', 'Destiny', 'Miracle Gliders', 'Birth', u'Tenohira -Show- (掌-show-)', u'Rinrei (凛麗)'], 'Gorgon City': ['Real', 'Intentions', 'Ready For Your Love', 'Here For You', 'Unmissable', 'Go All Night', 'Imagination', 'Saving My Life', 'The Crypt', 'Lover Like You'], 'Kano': ['Ps and Qs', 'Typical Me', 'Remember Me', 'Buss It Up', 'This Is The Girl', 'Hustler', 'Rock n Roller', 'Get Wild', 'Hail'], 'The Killers': ['Mr. Brightside', 'Somebody Told Me', 'Smile Like You Mean It', 'When You Were Young', 'Read My Mind', 'Human'], 'Nero': ['Act Like You Know', 'Innocence', 'Me & You', 'Guilt', 'Promises', 'Crush On You', 'Satisfy', 'Two Minds'] , 'Rudimental': ['Feel The Love', 'Waiting All Night', 'Right Here', 'Free', 'Powerless', 'Bloodstream', 'Lay It All On Me'], 'TRF': ['lights and any more', 'iNNOVATION', 'do you wanna dance?', 'virtual sea', 'Charge My Life', 'Broken Flower']}
 
 @app.route("/")
 def root():
@@ -10,7 +13,7 @@ def root():
 
 @app.route("/artists/")
 def artists():
-    artists = albums
+    artists = ['Avicii', 'Bugzy Malone', 'Calvin Harris', 'Clean Bandit', 'Drake', 'Eri Kitamura', 'Gorgon City', 'Kano', 'The Killers', 'Nero', 'Rudimental', 'TRF']
     return render_template('artists.html', artists = artists)
 
 @app.route("/artists/<artist>")
@@ -19,6 +22,7 @@ def get_artist(artist):
 
 @app.route("/albums/")
 def albums():
+    albums = sorted(['True', 'Walk With Me', 'I Created Disco', 'Ready For The Weekend', '18 Months', 'Motion', 'New Eyes', '2010', 'OriginL ClassC', 'Live fron France', 'Thank Me Later', 'Take Care', 'Nothing Was The Same', 'Re;Story', u'Shomei (証×明)', 'Sirens', 'The Crypt', 'Real', 'Home Sweet Home', 'London Town', '140 Grime Street', 'Method To The Meadness', 'Welcome Reality', 'Between II Worlds', 'Home', 'We The Generation', 'Hot Fuss', 'Sams Town', 'Day & Age', 'Battle Born', 'GRAVITY'])
     return render_template('albums.html', albums = albums)
 
 @app.route("/albums/<album>")
