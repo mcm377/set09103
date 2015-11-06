56024a5e1476cfab52d495f3028ee85aba73deec
mcm377 <40091038@live.napier.ac.uk>
2015-11-06 13:51:56 +0000

    Changed data structures Removed html files for artists and albums (pages now dynamically produced from dictionaries)

--- coursework1/src/catalogue.py
+++ coursework1/src/catalogue.py
@@ -20,7 +20,7 @@ def artists():
 
 @app.route("/artists/<artist>")
 def get_artist(artist):
-    return render_template('artist_details.html', artist = artist, artist_albums = artist_albums[artist], artist_tracks = artist_tracks[artist])
+    return render_template('artist_details.html', artist = artist, artist_albums = artist_albums[artist], artist_tracks = artist_tracks[artist], artist_genres = artist_genres[artist])
 
 @app.route("/albums/")
 def albums():
